9694b294fd45a8ab8ec89dddd446dcc0aa82f0bc
Koji NAKAMURA <kozy4324@gmail.com>
2024-11-29 16:13:06 +0900

    Initial implementation

--- test/ruby/lsp/test_rake.rb
+++ test/ruby/lsp/test_rake.rb
@@ -2,12 +2,16 @@
 
 require "test_helper"
 
-class Ruby::Lsp::TestRake < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::Ruby::Lsp::Rake::VERSION
-  end
+module Ruby
+  module Lsp
+    class TestRake < Minitest::Test
+      def test_that_it_has_a_version_number
+        refute_nil ::Ruby::Lsp::Rake::VERSION
+      end
 
-  def test_it_does_something_useful
-    assert false
+      def test_it_does_something_useful
+        assert true
+      end
+    end
   end
 end
