462ad7261a2b58fae23f22657dd82da3f80c16fe
Koji NAKAMURA <kozy4324@gmail.com>
2024-11-29 16:01:40 +0900

    Initial commit

---
+++ lib/ruby/lsp/rake.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "rake/version"
+
+module Ruby
+  module Lsp
+    module Rake
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
